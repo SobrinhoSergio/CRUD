9113706c701c10ac59702adc6b7d9e52afcf54ea
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2020-07-15 12:02:27 -0300

    Add a funcionalidade de atualizar os dados

--- includes/header.php
+++ includes/header.php
@@ -11,5 +11,6 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <title>Sistema de Cadastro de Clientes</title>
+    
 
     <body>
---
+++ php_action/update.php
@@ -0,0 +1,44 @@
+<?php
+
+//sesão
+session_start();
+
+//Conexão 
+require_once 'db-connect.php';
+
+if(isset($_POST['btn-editar'])):
+
+    if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
+        
+        $_SESSION['mensagem'] = "Erro ao Atualizar !";
+        header('Location: ../index.php');
+        
+    else:
+    
+        $nome = mysqli_escape_string($connect, $_POST['nome']);
+        $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
+        $email = mysqli_escape_string($connect, $_POST['email']);
+        $idade = mysqli_escape_string($connect, $_POST['idade']);
+
+        $id = mysqli_escape_string($connect, $_POST['id']);
+
+        $sql = "UPDATE clientes SET nome='$nome', sobrenome='$sobrenome', email='$email', idade='$idade' WHERE id = '$id'";
+
+            if(mysqli_query($connect, $sql)):
+        
+                $_SESSION['mensagem'] = "Atualizado com Sucesso !";
+                header('Location: ../index.php?');
+
+            else:
+
+                $_SESSION['mensagem'] = "Erro ao Atualizar !";
+                header('Location: ../index.php?');
+
+            endif;
+
+    endif;
+
+endif;
+
+
+?>
